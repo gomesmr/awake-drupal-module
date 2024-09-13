7ebe24a8b09618cae080cb1f8f84715e64121d0e
gomesmr <gomes.mr@gmail.com>
2024-09-13 11:53:11 -0300

    Clean module

--- src/Controller/AwakeController.php
+++ src/Controller/AwakeController.php
@@ -17,28 +17,6 @@ class AwakeController extends ControllerBase {
     return new static($container->get('awake.client'));
   }
 
-  public function postData() {
-    $payload = [
-      'products' => [['gtin' => '7896075300205', 'price' => '36.89']],
-      'company' => ['companyName' => 'Company'],
-      'user' => ['userName' => 'user'],
-    ];
-
-    $response = $this->awakeClient->connect('post', '/awake', [], $payload);
-    return $response ? [
-      '#theme' => 'awake_compare_response',
-      '#products' => $response['products'],
-      '#company' => $response['company'],
-      '#user' => $response['user'],
-      '#dateTime' => $response['dateTime'],
-    ] : ['#markup' => 'Error occurred'];
-  }
-
-  public function getProductByGtin($gtin) {
-    $response = $this->awakeClient->connect('get', '/awake-product/' . $gtin);
-    return $response ? ['#theme' => 'awake_get_product_by_id', '#product' => $response] : ['#markup' => 'Product not found'];
-  }
-
   public function renderResponse() {
     // Recupera os dados armazenados na sessão
     $response_data = \Drupal::request()->getSession()->get('awake_response_data');
